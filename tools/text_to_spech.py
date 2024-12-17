4e9b15a554077fd062ef8707d66bb90bfe632784
theotime2005 <theotime.berthod@epitech.digital>
2024-12-17 11:48:29 +0100

    feat: Using vlc to say texte

--- tools/text_to_spech.py
+++ tools/text_to_spech.py
@@ -1,18 +1,27 @@
 import sys
 from gtts import gTTS
-from playsound import playsound
 import tempfile
+import subprocess
 
-def text_to_speech(text):
+def text_to_speech(text, volume=400):
     """
-    Énonce le texte en utilisant gTTS et playsound.
+    Énonce le texte en utilisant gTTS et VLC, avec un contrôle du volume.
+    :param text: Le texte à énoncer.
+    :param volume: Le volume (0 à 512 pour VLC).
     """
+    gain = volume / 100
     tts = gTTS(text, lang="fr")
     with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as tmp_file:
         tts.save(tmp_file.name)
-        playsound(tmp_file.name)
-
+        # Utiliser VLC en ligne de commande pour lire le fichier audio avec volume ajusté
+        subprocess.run([
+            "cvlc", "--intf", "dummy", "--play-and-exit", f"--gain={gain}", tmp_file.name
+        ], check=True)
 
 # Exemple d'utilisation
 if __name__ == "__main__":
-    text_to_speech(sys.argv[1])
+    if len(sys.argv) > 2:
+        volume = int(sys.argv[2])  # Si un volume est fourni, on le récupère
+    else:
+        volume = 100  # Volume par défaut
+    text_to_speech(sys.argv[1], volume)
